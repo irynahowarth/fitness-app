69f2f6d3b83f0aa64305c867e25abb8e6cc8a900
Iryna Howarth <irynahowarth@gmail.com>
2024-01-18 10:17:46 +0000

    [refactor]: add additional data to MigrateJS server. add changes to WorkoutDetails to view new data.

--- src/Pages/WorkoutDetails.jsx
+++ src/Pages/WorkoutDetails.jsx
@@ -12,12 +12,34 @@ export default function WorkoutDetails() {
       .then((data) => setWorkout(data.workouts));
   }, [params.id]);
 
+  const circuits = workout?.circuits.map((circuit) => {
+    return (
+      <div key={crypto.randomUUID()}>
+        {circuit.name}
+        <span>{circuit.exercises.length} Exercises</span>
+        <span>{circuit.time} minutes</span>
+        <div>
+          {circuit.exercises.map((exercise) => {
+            return (
+              <div key={crypto.randomUUID()}>
+                <h2>{exercise.name}</h2>
+                {exercise.time && <span>00:{exercise.time}</span>}
+                {exercise.reps && <span>{exercise.reps} reps</span>}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  });
+
   return workout ? (
     <div>
       <h1>Workout Name:{workout.name}</h1>
-      <span>Circuits:{workout.circuits}</span>
-      <span>Exercises: {workout.exercises}</span>
+      <span>Circuits:{workout.circuits.length}</span>
+      <span>Exercises: </span>
       <span>Time: {workout.time}</span>
+      <div>{circuits}</div>
     </div>
   ) : (
     <h2>Loading....</h2>
--- src/server.jsx
+++ src/server.jsx
@@ -3,12 +3,16 @@ import { createServer, Model } from "miragejs";
 createServer({
   models: {
     workouts: Model,
+    exercise: Model,
   },
   routes() {
     this.namespace = "api";
 
     this.get("/workouts", (schema, request) => {
       return schema.workouts.all();
+    });
+    this.get("/exercises", (schema, request) => {
+      return schema.exercises.all();
     });
 
     this.get("/workouts/:id", (schema, request) => {
@@ -20,30 +24,368 @@ createServer({
     server.create("workout", {
       id: 1,
       name: "Full Body Circuit",
-      circuits: 4,
-      exercises: 14,
+      circuits: [
+        {
+          name: "Circuit 1",
+          time: 7,
+          exercises: [
+            {
+              name: "Ab Bikes",
+              reps: 40,
+            },
+            {
+              name: "Russian Twist",
+              reps: 30,
+            },
+            {
+              name: "Mountain Climer",
+              reps: 30,
+            },
+            {
+              name: "Heel Tap",
+              reps: 30,
+            },
+          ],
+        },
+        {
+          name: "Circuit 2",
+          time: 7,
+          exercises: [
+            {
+              name: "Bend leg jackknife",
+              reps: 20,
+            },
+            {
+              name: "Straight leg jackknife",
+              reps: 20,
+            },
+            {
+              name: "Side plank & Oblique crunch",
+              reps: 20,
+            },
+            {
+              name: "Skipping",
+              reps: 100,
+            },
+          ],
+        },
+        {
+          name: "Circuit 3",
+          time: 7,
+          exercises: [
+            {
+              name: "Ab Bikes",
+              reps: 40,
+            },
+            {
+              name: "Russian Twist",
+              reps: 30,
+            },
+            {
+              name: "Mountain Climer",
+              reps: 30,
+            },
+            {
+              name: "Heel Tap",
+              reps: 30,
+            },
+          ],
+        },
+        {
+          name: "Circuit 4",
+          time: 7,
+          exercises: [
+            {
+              name: "Bend leg jackknife",
+              reps: 20,
+            },
+            {
+              name: "Straight leg jackknife",
+              reps: 20,
+            },
+            {
+              name: "Side plank & Oblique crunch",
+              reps: 20,
+            },
+            {
+              name: "Skipping",
+              reps: 100,
+            },
+          ],
+        },
+      ],
       time: "28-35 Mins",
     });
     server.create("workout", {
       id: 2,
       name: "Full Body Bootcamp",
-      circuits: 6,
-      exercises: 13,
+      circuits: [
+        {
+          name: "Warm up",
+          time: 3,
+          exercises: [
+            {
+              name: "Criss Cross",
+              time: 20,
+            },
+            {
+              name: "High Knees",
+              time: 20,
+            },
+            {
+              name: "3-Way Mountain Climer",
+              time: 20,
+            },
+          ],
+        },
+        {
+          name: "Triset 1",
+          time: 6.5,
+          exercises: [
+            {
+              name: "Double-Pulse Jump Squat",
+              time: 30,
+            },
+            {
+              name: "Walking Lunge",
+              time: 30,
+            },
+            {
+              name: "Glute Bridge",
+              time: 30,
+            },
+          ],
+        },
+        {
+          name: "Triset 2",
+          time: 6.5,
+          exercises: [
+            {
+              name: "Double-Pulse Push-Up",
+              time: 30,
+            },
+            {
+              name: "X Plank",
+              time: 30,
+            },
+            {
+              name: "Plank",
+              time: 30,
+            },
+          ],
+        },
+        {
+          name: "Triset 3",
+          time: 6.5,
+          exercises: [
+            {
+              name: "Bent-Leg Raise",
+              time: 30,
+            },
+            {
+              name: "Alternating Superman",
+              time: 30,
+            },
+            {
+              name: "Flutters",
+              time: 30,
+            },
+          ],
+        },
+        {
+          name: "Superset 1",
+          time: 2,
+          exercises: [
+            {
+              name: "Skaters",
+              time: 30,
+            },
+            {
+              name: "Squat Pulse",
+              time: 30,
+            },
+          ],
+        },
+        {
+          name: "Superset 2",
+          time: 2,
+          exercises: [
+            {
+              name: "Half Burpee & Push-Up",
+              time: 30,
+            },
+            {
+              name: "Shoulder Tap",
+              time: 30,
+            },
+          ],
+        },
+      ],
       time: "28-35 Mins",
     });
     server.create("workout", {
       id: 3,
       name: "Arm & Abs",
-      circuits: 4,
-      exercises: 8,
+      circuits: [
+        {
+          name: "Circuit 1",
+          time: 7,
+          exercises: [
+            {
+              name: "X Plank",
+              reps: 20,
+            },
+            {
+              name: "Mountain Climber Push-Up (4:1)",
+              reps: 10,
+            },
+            {
+              name: "Straight-Leg Jackknife",
+              reps: 12,
+            },
+            {
+              name: "Side Plank & Oblique Crunch",
+              reps: 20,
+            },
+          ],
+        },
+        {
+          name: "Circuit 2",
+          time: 7,
+          exercises: [
+            {
+              name: "Side-to-Side Half Burpee",
+              reps: 16,
+            },
+            {
+              name: "Tricep Push-Up",
+              reps: 10,
+            },
+            {
+              name: "Russian Twist",
+              reps: 30,
+            },
+            {
+              name: "Ab bikes",
+              reps: 30,
+            },
+          ],
+        },
+        {
+          name: "Circuit",
+          time: 7,
+          exercises: [
+            {
+              name: "X Plank",
+              reps: 20,
+            },
+            {
+              name: "Mountain Climber Push-Up (4:1)",
+              reps: 10,
+            },
+            {
+              name: "Straight-Leg Jackknife",
+              reps: 12,
+            },
+            {
+              name: "Side Plank & Oblique Crunch",
+              reps: 20,
+            },
+          ],
+        },
+        {
+          name: "Circuit 4",
+          time: 7,
+          exercises: [
+            {
+              name: "Side-to-Side Half Burpee",
+              reps: 16,
+            },
+            {
+              name: "Tricep Push-Up",
+              reps: 10,
+            },
+            {
+              name: "Russian Twist",
+              reps: 30,
+            },
+            {
+              name: "Ab bikes",
+              reps: 30,
+            },
+          ],
+        },
+      ],
       time: "28-35 Mins",
     });
     server.create("workout", {
       id: 4,
       name: "Express Legs",
-      circuits: 0,
-      exercises: 7,
+      circuits: [
+        {
+          name: "Circuit",
+          time: 12,
+          exercises: [
+            {
+              name: "Modified Jump Lunge",
+              time: 30,
+            },
+            {
+              name: "High Knees",
+              time: 30,
+            },
+            {
+              name: "Sumo Squat",
+              time: 30,
+            },
+            {
+              name: "Skaters",
+              time: 30,
+            },
+            {
+              name: "Glute Bridge",
+              time: 30,
+            },
+            {
+              name: "Burpee",
+              time: 30,
+            },
+            {
+              name: "Bent-Leg Sit-Up & Reach",
+              time: 30,
+            },
+            {
+              name: "Rest",
+              time: 30,
+            },
+          ],
+        },
+      ],
       time: "12-15 Mins",
+    });
+    server.create("exercise", {
+      name: "Burpee",
+      desc: "",
+      equip: [],
+      areas: [],
+    });
+    server.create("exercise", {
+      name: "X Plank",
+      desc: "",
+      equip: [],
+      areas: [],
+    });
+    server.create("exercise", {
+      name: "Skipping",
+      desc: "",
+      equip: ["skipping rope"],
+      areas: [],
+    });
+    server.create("exercise", {
+      name: "Russian Twist",
+      desc: "",
+      equip: [],
+      areas: [],
     });
   },
 });
